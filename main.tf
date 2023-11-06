5d2c4d6737cc419c17fef6deb0ad53c4a1bc72ce
Ubuntu <ubuntu@ip-172-31-25-27.us-east-2.compute.internal>
2023-11-06 22:41:32 +0000

    new files

---
+++ main.tf
@@ -0,0 +1,218 @@
+#Provider configuration for AWS
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# Create a VPC
+resource "aws_vpc" "docker_vpc" {
+  cidr_block = var.vpc_cidr
+}
+
+
+#creating internet gateway
+resource "aws_internet_gateway" "docker_igw" {
+  vpc_id = aws_vpc.docker_vpc.id
+
+}
+
+# Define two public subnets
+resource "aws_subnet" "public_subnet" {
+  count = length(var.public_subnet_availability_zone)
+  vpc_id = aws_vpc.docker_vpc.id
+  cidr_block = var.public_subnet_cidr[count.index]
+  availability_zone = var.public_subnet_availability_zone[count.index]
+  map_public_ip_on_launch = true  # This enables auto-assigning public IPs
+  tags = {
+    Name = "Public Subnet ${count.index}"
+  }
+}
+
+
+# Define two private subnets
+resource "aws_subnet" "private_subnet" {
+  count = length(var.private_subnet_availability_zone)
+  vpc_id = aws_vpc.docker_vpc.id
+  cidr_block = var.private_subnet_cidr[count.index]
+  availability_zone = var.private_subnet_availability_zone[count.index]
+  map_public_ip_on_launch = false  # Private subnets don't auto-assign public IPs
+  tags = {
+    Name = "Private Subnet ${count.index}"
+  }
+}
+
+
+# Create a security group for your Docker registry
+resource "aws_security_group" "docker_registry_sg" {
+name = "security"
+description = "Security group for Docker registry EC2 instance"
+vpc_id = aws_vpc.docker_vpc.id
+
+  # Define ingress and egress rules as needed for your use case
+  # For example, you might need to open port 5000 for your Docker registry
+ ingress {
+    from_port   = var.docker_registry_ingress_port
+    to_port     = var.docker_registry_ingress_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Open port 22 for SSH (you might want to restrict this for production)
+  #ingress {
+  # from_port   = 22
+  #to_port     = 22
+  #protocol    = "tcp"
+  #cidr_blocks = ["0.0.0.0/0"]
+  #}
+
+  # Define egress rules as needed
+  egress {
+    from_port   = 0
+    to_port     = 0     # Allow all outbound traffic
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]    # Allow traffic to any destination
+  }
+}
+
+# Create an EC2 instance for your Docker registry
+#resource "aws_instance" "docker_registry" {
+#  ami             = var.docker_registry_ami
+#  instance_type   = var.docker_registry_instance_type
+#  subnet_id       = aws_subnet.private_subnet[count.index].id
+#  security_groups = [aws_security_group.docker_registry_sg.id]
+ # count = length(var.private_subnet_availability_zone)
+  
+
+  # You can customize the user data script to set up your Docker registry on launch
+  #user_data = var.docker_registry_user_data
+#}
+
+
+
+resource "aws_eks_cluster" "pythonapp_cluster" {
+  name     = "python-eks-cluster"
+  role_arn = aws_iam_role.eks_clusterrole.arn
+  vpc_config {
+    subnet_ids = [for subnet in aws_subnet.public_subnet : subnet.id]
+  }
+  depends_on = [aws_eks_cluster.pythonapp_cluster]
+}
+
+
+#Create IAM roles for your EKS cluster and worker nodes
+resource "aws_iam_role" "eks_clusterrole" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+
+
+resource "aws_eks_node_group" "pythonapp_workers" {
+  cluster_name    = aws_eks_cluster.pythonapp_cluster.name
+  node_group_name = var.eks_node_group_name
+  node_role_arn   = aws_iam_role.eks_nodesrole.arn
+  subnet_ids      = [for i in aws_subnet.private_subnet : i.id]
+  count = length(var.private_subnet_availability_zone)
+  instance_types  =  var.docker_registry_instance_type # Modify as needed
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+}
+
+resource "aws_iam_role" "eks_nodesrole" {
+  name = "eks-nodes-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+
+#To grant the necessary IAM permissions to the EKS node group to join an EKS cluster, create a IAM policy and give list actions of what the Nodegroup can do in the cluster
+resource "aws_iam_policy" "eks_nodegroup_policy" {
+  name        = "eks-nodegroup-policy"
+  description = "IAM policy for EKS node group to join the cluster"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "eks:DescribeCluster",
+          "eks:ListNodegroups",
+          "eks:CreateNodegroup",
+          "eks:TagResource",  # Add more permissions as needed
+        ],
+        Effect   = "Allow",
+        Resource = "*",
+      },
+    ],
+  })
+}
+
+
+
+#Attach the IAM policy to the node group's IAM role. You can use the aws_iam_policy_attachment resource to do this:
+resource "aws_iam_policy_attachment" "eks_nodegroup_attachment" {
+  name       = "eks-nodegroup-attachment"
+  count      = length(aws_eks_node_group.pythonapp_workers)   #This ensures that the aws_iam_policy_attachment resource is created for each instance of the node group
+  roles      = [aws_eks_node_group.pythonapp_workers.node_group_name]
+  policy_arn = aws_iam_policy.eks_nodegroup_policy.arn
+}
+
+
+
+
+
+
+
+#To authenticate with your EKS cluster, create an authentication config using a data block instead of resource block
+#resource "aws_eks_cluster_auth" "pythonapp_cluster_auth" {
+#  name = aws-eks-auth
+#}
+
+data "aws_eks_cluster_auth" "pythonapp_cluster_auth" {
+name = "python-eks-cluster"
+#region = "us-east-2"
+
+}
+
+
+
+
+
+
+
+
---
+++ variable.tf
@@ -0,0 +1,122 @@
+variable "aws_region" {
+  description = "AWS region where the resources will be created"
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+
+variable "public_subnet_cidr" {
+  description = "CIDR blocks for the public subnets"
+   type        = list(string)
+  default     = ["10.0.0.0/24", "10.0.1.0/24"]
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR blocks for the private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "public_subnet_availability_zone" {
+  description = "Availability zone for the public subnet"
+   type        = list(string)
+  default     = ["us-west-2a", "us-west-2b"]
+}
+
+variable "private_subnet_availability_zone" {
+  description = "Availability zone for the private subnet"
+   type        = list(string)
+  default     = ["us-west-2c", "us-west-2d"]
+}
+
+variable "docker_registry_ingress_port" {
+  description = "Port for Docker registry ingress"
+  default     = 5000
+}
+
+variable "docker_registry_ami" {
+  description = "AMI for the Docker registry EC2 instance"
+  default     = "ami-083ac598d805fbb10" # for ubuntu 20
+
+}
+
+variable "docker_registry_instance_type" {
+  description = "EC2 instance type for the Docker registry"
+  type        = list(string)
+  default     = ["t2.medium"]
+}
+
+# EKS Cluster Name
+variable "eks_cluster_name" {
+  description = "The name of the EKS cluster."
+  type        = string
+  default     = "python-eks-cluster" # You can change this to your desired cluster name
+}
+
+
+# EKS Node Group Settings
+variable "eks_node_group_name" {
+  description = "The name of the EKS node group."
+  type        = string
+  default     = "pythonapp-node-group" # You can change this to your desired node group name
+}
+
+
+#variable "eks_node_instance_type" {
+#  description = "The EC2 instance type for EKS worker nodes."
+ # type        = string
+ # default     = "t2.micro" # Modify as needed
+#}
+
+variable "eks_node_desired_size" {
+  description = "The desired number of worker nodes in the EKS node group."
+  type        = number
+  default     = 1
+}
+
+variable "eks_node_max_size" {
+  description = "The maximum number of worker nodes in the EKS node group."
+  type        = number
+  default     = 2
+}
+
+variable "eks_node_min_size" {
+  description = "The minimum number of worker nodes in the EKS node group."
+  type        = number
+  default     = 1
+}
+
+
+#variable "aws_eks_cluster_auth" {
+#description = "eks cluster authentification "  
+#type        =   string
+#default     =  
+
+#}
+
+
+
+
+#variable "docker_registry_user_data" {
+#  description = "User data script for setting up the Docker registry"
+#  default = <<-EOF
+#              #!/bin/bash
+#              # Your user data script here for setting up the Docker registry
+#              EOF
+#}
+
+#variable "aws_subnet_private_subnet_docker_id" {
+#description = "ID of the private subnet used for Docker in the VPC."
+#  type        = string
+#  default     = "subnet-0747b57d1d6a387c6"
+#}
+
+#variable "aws_subnet_public_subnet_docker_id" {
+#description = "ID of the public subnet used for Docker in the VPC."
+#  type        = string
+#  default     = "subnet-05a4cbfe9097176e1"
+#}
